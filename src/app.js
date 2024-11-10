591d2b7aeddbeaf64d1f06e77730eb4c241cb392
aftlah <altaf.fattah05@gmail.com>
2024-11-10 22:01:10 +0700

    change variabel name

--- src/app.js
+++ src/app.js
@@ -3,7 +3,7 @@ const cors = require('cors');
 const bodyParser = require('body-parser');
 const voteRoutes = require('./routes/voteRoutes');
 const candidateRoutes = require('./routes/candidateRoutes');
-const voterRoutes = require('./routes/authRoutes'); 
+const authRoutes = require('./routes/authRoutes'); 
 require('dotenv').config();
 
 const app = express();
@@ -14,7 +14,7 @@ app.use(bodyParser.json());
 
 app.use('/api/afx-v1/votes', voteRoutes);
 app.use('/api/afx-v1/candidates', candidateRoutes);
-app.use('/api/afx-v1/auth', voterRoutes); 
+app.use('/api/afx-v1/auth', authRoutes); 
 
 app.listen(port, () => {
     console.log(`Server running at http://localhost:${port}`);
